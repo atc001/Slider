c06f6500efeb04366b3ee80b5fc0cb985a300cc9
atc001 <coste.anthony.ca@gmail.com>
2017-06-28 12:07:43 +0000

    Added creation of slider, images are hidden, except for the first one

--- slider.js
+++ slider.js
@@ -1,10 +1,34 @@
 $(document).ready(function() {
-    
+
+    var sliderHtml = "";
+    var sliderData;
+    var height = "500";
+
     initializeSlider();
+    createSlider();
+    render();
+
+    function createSlider() {
+        sliderHtml += "<div id='slider-container' style='display:flex;flex-direction:row;overflow:hidden;'>"
+        for (var i = 0; i < sliderData.length; i++) {
+            var display;
+            if(i == 0) {
+                display = "display:flex;justify-content:center";
+            } else {
+                display = "display:none";
+            }
+            sliderHtml += "<div style='width:100%;height:500px;" +  display + "'>";
+            sliderHtml += "<img src='" + $(sliderData[i])[0].src + "' style='height:" + height + "px;width:auto;'>";
+            sliderHtml += "</div>"
+        }
+        sliderHtml += "</div>"
+        sliderHtml += "<button data-action='left'><</button>";
+        sliderHtml += "<button data-action='right'>></button>";
+    }
 
     function initializeSlider() {
         // Load the html hook
-        var sliderData = $('#slider-container img');
+        sliderData = $('#slider-container img');
 
         // If the hook is broken, throw error to stop script
         if(sliderData.length == 0) {
@@ -13,4 +37,7 @@ $(document).ready(function() {
         $('#slider-container').remove();
     }
     
+    function render() {
+        $('body').append(sliderHtml);
+    }
 });
